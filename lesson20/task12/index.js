47f19e506824918afae10304e454c87d627109ff
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-23 16:06:32 +0300

    add export

--- lesson20/task12/index.js
+++ lesson20/task12/index.js
@@ -1,4 +1,4 @@
-function squaredNumbers() {
+export function squaredNumbers() {
   const numbers = Array.from(document.querySelectorAll(`.number`));
   numbers.map((el) => {
     el.setAttribute(`data-squared-number`, el.dataset.number ** 2);
