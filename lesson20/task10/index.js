fffe92904e18e425a6f2e6a90824e376bdca85b4
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-23 13:31:34 +0300

    created

---
+++ lesson20/task10/index.js
@@ -0,0 +1,10 @@
+export function finishForm() {
+  const inputText = document.createElement(`input`);
+  inputText.setAttribute(`type`, `text`);
+  inputText.setAttribute(`name`, `login`);
+  document.querySelector(`form`).prepend(inputText);
+
+  document
+    .querySelector(`input[name = "password"]`)
+    .setAttribute(`type`, `password`);
+}
