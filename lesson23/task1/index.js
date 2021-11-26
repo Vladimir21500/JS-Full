5d37d111149b6413d9de22dcfabaa4ecc34f4cd4
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-26 13:39:29 +0300

    add export

--- lesson23/task1/index.js
+++ lesson23/task1/index.js
@@ -1,6 +1,6 @@
 const weekDays = [`Mo`, `Tu`, `We`, `Th`, `Fr`, `St`, `Su`];
 
-function dayOfWeek(date, days) {
+export function dayOfWeek(date, days) {
   const day = new Date(date).getDate();
   const dateInFuture = new Date(date).setDate(day + days);
 
