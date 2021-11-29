f6364b16217fe11bb2ad0db75e1cc3652f6a33b2
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-29 18:23:52 +0300

    created

---
+++ lesson22/task3/src/scripts/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+
+import { initTodoListHandlers } from './todoList.js';
+import { renderTasks } from './renderer.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderTasks();
+  initTodoListHandlers();
+});
