880ce33ed8d0792db93e598d3f9903f7e865049a
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-29 14:37:33 +0300

    created

---
+++ lesson24/task1/index.js
@@ -0,0 +1,22 @@
+localStorage.clear();
+localStorage.setItem(`hobbies`, JSON.stringify({ name: `Tom` }));
+localStorage.setItem(`name`, JSON.stringify(`Tom`));
+localStorage.setItem(`age`, JSON.stringify(17));
+localStorage.setItem(`isStudent`, JSON.stringify(true));
+localStorage.setItem(`car`, JSON.stringify(null));
+
+export const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
+};
