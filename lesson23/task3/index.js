3f15bdec8d06c22da76ddb1a40ba70761fc4f02f
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-29 12:10:12 +0300

    new algoritme

--- lesson23/task3/index.js
+++ lesson23/task3/index.js
@@ -1,59 +1,19 @@
-/* 
-найти разницу во времени в формате d h m s
-алгоритм 
-in: date1, date2
-out: разница
-
-алгоритм: 
-1. перевести даты в миллисекунды и найти разницу
-2. перевести разницу в нужный формат
-2.1 по порядку добавлять в ответ и переводить в дни полный год,месяцы, день
-                                              в минуты 
-                                              секунды  
-так как в месяце не одинаковое кол-во дней создадим обькт
-с которого будем считывать кол-во дней в зависимости от месяца                                                                           
-*/
-const DEFAULT_POINT_YEAR = 1970;
-const DAYS_YEAR = 365;
-
-const months = {
-  jan: 31,
-  feb: 28,
-  mar: 31,
-  apr: 30,
-  may: 31,
-  jun: 30,
-  jul: 31,
-  auh: 31,
-  sep: 30,
-  oct: 31,
-  nov: 30,
-  dec: 31,
-  getSumDays(year, month) {
-    if (year % 4 === 0) this.feb = 29;
-
-    return Object.values(this).reduce((acc, el, index) => {
-      if (index < month) {
-        return acc + el;
-      }
-      return acc;
-    }, 0);
-  },
-};
-
 export function getDiff(startDate, endDate) {
-  const diff = Math.abs(
+  const DAYS_IN_MS = 86400000;
+  const HOURS_IN_MS = 3600000;
+  const MINUTES_IN_MS = 60000;
+  const SECONDS_IN_MS = 1000;
+
+  let diff = Math.abs(
     new Date(endDate).getTime() - new Date(startDate).getTime()
   );
-  const fullYear = new Date(diff).getUTCFullYear() - DEFAULT_POINT_YEAR;
-  const fullMonths = new Date(diff).getUTCMonth();
 
-  let days = fullYear * DAYS_YEAR;
-  days += months.getSumDays(fullYear, fullMonths);
-  days += new Date(diff).getUTCDate() - 1;
-
-  const hours = new Date(diff).getUTCHours();
-  const minutes = new Date(diff).getUTCMinutes();
-  const seconds = new Date(diff).getUTCSeconds();
+  const days = Math.floor(diff / DAYS_IN_MS);
+  diff -= days * DAYS_IN_MS;
+  const hours = Math.floor(diff / HOURS_IN_MS);
+  diff -= hours * HOURS_IN_MS;
+  const minutes = Math.floor(diff / MINUTES_IN_MS);
+  diff -= minutes * MINUTES_IN_MS;
+  const seconds = Math.floor(diff / SECONDS_IN_MS);
   return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
