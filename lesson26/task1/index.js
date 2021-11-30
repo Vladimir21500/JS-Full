b920ab3be2183f3b1b37e0a72c667e89c92bf070
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-30 15:07:49 +0300

    change few names

--- lesson26/task1/index.js
+++ lesson26/task1/index.js
@@ -14,17 +14,17 @@ export const addImage = (imgSrc, callback) => {
   imgElem.addEventListener('error', () => callback('Image load is failed'));
 };
 
-const onImageLoaded = (error, data) => {
+const onImageLoaded = (error, imgElem) => {
   if (error) {
     console.log(error);
     return;
   }
-  const { width, height } = data;
+  const { width, height } = imgElem;
   const sizeElem = document.querySelector('.image-size');
   sizeElem.textContent = `${width} x ${height}`;
 };
 
-addImage(
+/* addImage(
   'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg',
   onImageLoaded
-);
+); */
