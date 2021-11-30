8e80fb3ccadc9128292acaef1a29ae265f1b853c
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-30 15:15:25 +0300

    create

--- lesson26/task3/index.js
+++ lesson26/task3/index.js
@@ -1,9 +1,25 @@
-const main = () => {
-  console.log('1');
-  setTimeout(() => console.log('2'), 0);
-  console.log('3');
-  setTimeout(() => console.log('4'), 1000);
-  console.log('5');
-};
+'use strict';
 
-main();
+/**
+ * @return {undefined}
+ */
+export const printing = () => {
+  console.log(1);
+
+  setTimeout(function () {
+    console.log(5);
+  }, 1000);
+
+  console.log(2);
+
+  setTimeout(function () {
+    console.log(4);
+  }, 0);
+
+  setTimeout(function () {
+    console.log(6);
+  }, 2000);
+
+  console.log(3);
+};
+printing();
