0ba6b60f8ca2a9a9b469e8e163d964ad3e0e8f16
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-11-30 06:18:43 +0300

    add export

--- lesson25/task2/index.js
+++ lesson25/task2/index.js
@@ -1,4 +1,23 @@
-const markFavorites = (tree, favorites) => {
+const favorites = ['id-2'];
+const tree = {
+  id: 'id-1',
+  name: 'Products',
+  nodes: [
+    {
+      id: 'id-2',
+      name: 'Food',
+      nodes: [
+        {
+          id: 'id-3',
+          name: 'Food',
+          nodes: [],
+        },
+      ],
+    },
+  ],
+};
+
+export const markFavorites = (tree, favorites) => {
   const isFavorite = favorites.includes(tree.id);
 
   return {
@@ -8,4 +27,6 @@ const markFavorites = (tree, favorites) => {
   };
 };
 
-const result = markFavorites;
+const result = markFavorites(tree, favorites);
+
+console.log(result);
