0ff50378f0f0c34b04674e0f71c73497f693de64
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-01 07:56:42 +0300

    created

---
+++ lesson27/task2/index.js
@@ -0,0 +1,30 @@
+export const addImageV2 = (imgSrc) => {
+  const p = new Promise((resolve, reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () =>
+      reject(new Error('Image load is failed'))
+    );
+  });
+
+  return p;
+};
+/* 
+addImageV2(
+  'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
+).then((data) => console.log(data)); // ==> { width: 200, height: 100 }
+addImageV2(
+  'ttps://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'
+).catch((error) => console.log(error)); // ==> 'Image load failed'
+ */
