322a3e5b1e4bb71520d5eb52824967e68eaedcb6
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-01 17:11:51 +0300

    created

--- lesson28/task2/index.js
+++ lesson28/task2/index.js
@@ -1,22 +1,21 @@
-const asyncCalculator = num =>
+export const asyncCalculator = number =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log(`Initial value: ${num}`);
-      resolve(num);
-    }, 3000);
+      console.log(`Initial value: ${number}`);
+      resolve(number);
+    }, 500);
   })
     .then(
       value =>
         new Promise(resolve => {
           setTimeout(() => {
             const result = value ** 2;
-            console.log(`Squared Value: ${result}`);
+            console.log(`Squared value: ${result}`);
             resolve(result);
-          }, 3000);
+          }, 500);
         }),
     )
     .then(value => {
       const result = value * 2;
       console.log(`Doubled value: ${result}`);
     });
-asyncCalculator(4).then(value => console.log(value));
