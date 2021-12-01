f3857c0e283d700501150449e7625bdea75f7239
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-01 19:06:01 +0300

    created

---
+++ lesson29/task1/index.js
@@ -0,0 +1,22 @@
+/* const getValueWithDelay = (value, delay) =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      console.log(value);
+      resolve(value);
+    }, delay);
+  });
+
+const asyncNum1 = getValueWithDelay(56, 1000);
+const asyncNum2 = getValueWithDelay(undefined, 2000);
+const asyncNum3 = getValueWithDelay('10', 2000); */
+
+const getSum = numbers =>
+  numbers.filter(value => !isNaN(value)).reduce((acc, num) => acc + Number(num), 0);
+
+const asyncSum = (...asyncNumbers) =>
+  Promise.all(asyncNumbers)
+    .then(numbers => getSum(numbers))
+    .catch(() => Promise.reject(new Error("Can't calculate")));
+
+/* asyncSum(asyncNum1, Promise.reject(), asyncNum3).then(result => console.log(result));
+ */
