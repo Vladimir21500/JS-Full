c8e28c479850c540016ccf037c00aeb7cd6cce1a
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-10 13:30:09 +0300

    add failed to load data

--- lesson33/task1/index.js
+++ lesson33/task1/index.js
@@ -1,10 +1,10 @@
-export const fetchUser = async userId => {
+const fetchUser = async userId => {
   try {
     const response = await fetch(`https://api.github.com/users/${userId}`);
     if (!response.ok) return null;
     const userData = await response.json();
     return userData;
   } catch (err) {
-    throw new Error();
+    throw new Error('Failed to load data');
   }
 };
